19a859dfbb615c3f8e263878fdcd31ff2924fc67
Rafa <kimtoba890@gmail.com>
2024-10-04 22:21:24 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,6 +0,0 @@
-def exibirMensagem(nome, idade):
-    print(f"Olá, {nome} com {idade} anos!")
-
-nome = input("Digite o seu nome: \n")
-idade = input("Digite a sua idade: \n")
-exibirMensagem(nome, idade) # Exibe a mensagem com o nome do usuário.
